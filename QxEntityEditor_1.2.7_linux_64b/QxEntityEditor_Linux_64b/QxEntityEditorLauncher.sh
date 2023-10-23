2e3b0cc2e10d8c4611a134428e85b11553f8288c
if-loop69420 <j.sztavi@pm.me>
2023-10-23 10:09:11 +0200

    Finally this is working again

--- QxEntityEditor_1.2.7_linux_64b/QxEntityEditor_Linux_64b/QxEntityEditorLauncher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-clear
-pwd
-cd ./app/
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$PWD/unixODBC/lib:$LD_LIBRARY_PATH
-./QxEntityEditor "$@"
